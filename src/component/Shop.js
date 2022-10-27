d63075e16fb8a1556c1680c328ee223ad7cae768
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2022-10-27 09:50:45 +0530

    completion of video 74

---
+++ src/component/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+    return (
+        <div className='container my-3'>
+            <h2>Buy Abibas Shoes 50 Rs</h2>
+            <button className="btn btn-primary mx-2">-</button>
+            Add this item to cart
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shop
